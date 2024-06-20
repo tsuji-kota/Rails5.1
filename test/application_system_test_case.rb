173b3961b6741878ed3e9365e6052c93f40cb1a0
tsuji-kota <tsuji.kota@mikilab.doshisha.ac.jp>
2024-06-20 11:24:29 +0900

    create dynamic_title

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
 end
