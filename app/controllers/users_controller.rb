58123937efb116a9878e79475ba8af8a9e265f0a
tsuji-kota <tsuji.kota@mikilab.doshisha.ac.jp>
2024-06-27 17:26:10 +0900

    style:add-newline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,7 +20,10 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-    :password_confirmation)
-    end
+    params.require(:user)
+          .permit(:name,
+                  :email, 
+                  :password, 
+                  :password_confirmation)
+  end
 end
