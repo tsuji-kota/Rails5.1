3f9c5c4b8c6665e761ca98d1e5b73406bcc037f5
tsuji-kota <tsuji.kota@mikilab.doshisha.ac.jp>
2024-06-27 14:47:42 +0900

    feat:add-redirect&flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
