c6c7320b3b006e33f1ead6354908513968b5dac1
tsuji-kota <tsuji.k214@gmail.com>
2024-07-18 23:30:02 +0900

    feat:redirect-to-login-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
   def new
     @user = User.new
   end
@@ -39,5 +40,13 @@ class UsersController < ApplicationController
                   :email, 
                   :password, 
                   :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 end
