980b314098b7ad016101a940874334300ec58ec4
tsuji-kota <tsuji.k214@gmail.com>
2024-07-19 19:37:07 +0900

    fix:typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true
+             admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
