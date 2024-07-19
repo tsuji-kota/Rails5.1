f460ae237b6fc17282a729c6ce58ad2801d5d3b4
tsuji-kota <tsuji.k214@gmail.com>
2024-07-19 14:25:30 +0900

    feat:test-pach-admin-true

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,4 +43,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "",
+                                            password_confirmation: "",
+                                            admin: True } }
+    assert_not @other_user.reload.admin?
+  end
 end
