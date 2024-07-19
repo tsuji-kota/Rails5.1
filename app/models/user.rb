5409b7e31557360f20b7ddcbdfe3b00bcf3357d9
tsuji-kota <tsuji.k214@gmail.com>
2024-07-19 20:14:10 +0900

    feat:show-only-activate-acount
    refactor:use-scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  scope :activated, -> { where(activated: true) }
 
   class << self
     def digest(string)
