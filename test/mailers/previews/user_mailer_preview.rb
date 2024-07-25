a964d28a7f733960ffb78c655ecde13a6352952f
tsuji-kota <tsuji.k214@gmail.com>
2024-07-25 13:53:15 +0900

    feat:mailer-method

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -10,7 +10,8 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 
 end
