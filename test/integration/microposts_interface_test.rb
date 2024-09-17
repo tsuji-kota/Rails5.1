15e80c8bfb75e966b1f7e99f83bde330e49e135c
tsuji-kota <tsuji.k214@gmail.com>
2024-09-17 18:42:59 +0900

    feat:test-picture

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,17 +10,17 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-
+    assert_select 'input[type=file]'
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
     end
     assert_select 'div#error_explanation'
-
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     content = "This micropost really ties the room together"
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
-    assert_redirected_to root_url
+    assert @user.microposts.first.picture?
     follow_redirect!
     assert_match content, response.body
 
